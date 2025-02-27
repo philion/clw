082c30cfe7ba3657e90c2fe2754a3af2f613e80a
Paul Philion <pphlilion@istreamplanet.com>
2025-02-27 10:38:50 -0800

    v0.5.1: Finally got the uvx run command working, by embedding the json weather codes in the code.

--- src/clw/app.py
+++ src/clw/app.py
@@ -154,7 +154,7 @@ class WeatherApp(App[None]):
 
 def main() -> None:
     """run the weather app"""
-    logging.basicConfig(level=logging.DEBUG)
+    #logging.basicConfig(level=logging.DEBUG)
     WeatherApp().run()
 
 
